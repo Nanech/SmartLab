ad79003306b5923eb769bc9a4b613cfcf3032d19
Nanech <99821880+Nanech@users.noreply.github.com>
2023-09-21 17:13:34 +0300

    implements nav & fragments, trying to do it

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,5 +46,10 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
+    // Navigation & Fragments
+    val nav_version = "2.7.3"
+    implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
+    implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
+    implementation("androidx.navigation:navigation-dynamic-features-fragment:$nav_version")
 
 }
