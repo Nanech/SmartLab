2e46d695af2f2c7027430c8372a6779fb74cf54b
unknown <@ngknn.local>
2023-09-22 13:19:18 +0300

    trouble with listenet in authFragment

--- app/src/main/java/com/example/smartlab/AuthFragment.kt
+++ app/src/main/java/com/example/smartlab/AuthFragment.kt
@@ -1,10 +1,14 @@
 package com.example.smartlab
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.example.smartlab.databinding.FragmentAuthBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,10 +20,14 @@ private const val ARG_PARAM2 = "param2"
  * Use the [AuthFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
+
 class AuthFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+
+    lateinit var binding: FragmentAuthBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,15 +35,56 @@ class AuthFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
+        binding = FragmentAuthBinding.inflate(layoutInflater)
+
+        binding.btn.setBackgroundResource(R.drawable.btn_enabled)
+
+        // Трабл со слушателем
+
+        binding.edEmail.addTextChangedListener(object:TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {   }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                if ( android.util.Patterns.EMAIL_ADDRESS.matcher( binding.edEmail.text.
+                    toString() ).matches()) {
+                    binding.btn.setBackgroundColor(R.drawable.btn_enabled)
+
+                    binding.btn.isEnabled = true
+                    Toast.makeText(activity  ,"Активирована", Toast.LENGTH_LONG).show()
+                } else {
+                    binding.btn.isEnabled = false
+                    binding.btn.setBackgroundColor(R.drawable.btn_nonenabled)
+                    binding.edEmail.setError("Invalid email")
+                }
+            }
+
+        } )
+
+
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_auth, container, false)
+
+    override fun onCreateView( inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?): View? {
+
+
+
+            // Inflate the layout for this fragment
+            return inflater.inflate(R.layout.fragment_auth, container, false)
+
+
     }
+
+
+
+
+
+
 
     companion object {
         /**
