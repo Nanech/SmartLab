6fc9d1740b1bd8a06eb23652bf3bb9e86240da42
Nanech <99821880+Nanech@users.noreply.github.com>
2023-10-01 19:38:34 +0300

    hide keyboard function, and start making api interface

--- app/src/main/java/com/example/smartlab/fragments/EmailCodeFragment.kt
+++ app/src/main/java/com/example/smartlab/fragments/EmailCodeFragment.kt
@@ -1,11 +1,16 @@
 package com.example.smartlab.fragments
 
+import android.app.Activity
+import android.content.Context
+import android.inputmethodservice.InputMethodService
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
+import androidx.core.content.getSystemService
 import androidx.core.widget.doOnTextChanged
 import androidx.navigation.fragment.findNavController
 import com.example.smartlab.R
@@ -31,10 +36,13 @@ class EmailCodeFragment : Fragment() {
 
         val view = binding.root
 
-
-
         binding.btnBack.setOnClickListener{
             findNavController().navigate(R.id.back_to_email)
+        }
+
+        binding.otpView.setOtpCompletionListener {
+            hideKeyboard()
+            Toast.makeText( requireContext() , "Entered pin: $it", Toast.LENGTH_SHORT).show()
         }
 
 
@@ -42,6 +50,21 @@ class EmailCodeFragment : Fragment() {
 
         return view
     }
+
+    fun Fragment.hideKeyboard() {
+        view?.let { activity?.hideKeyboard(it) }
+    }
+
+    fun Activity.hideKeyboard() {
+        hideKeyboard(currentFocus ?: View(this))
+    }
+
+    fun Context.hideKeyboard(view: View) {
+        val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+
 
 
 
